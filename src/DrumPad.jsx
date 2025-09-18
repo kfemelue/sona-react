24ce79e114bf8ffe5cb25f953bf1746a961c7da9
kfemelue <kfemelue@gmail.com>
2025-09-18 10:51:24 -0500

    Add key press event to current drumpad

--- src/DrumPad.jsx
+++ src/DrumPad.jsx
@@ -1,69 +1,80 @@
-import { useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Song, Track, Instrument, Effect } from 'reactronica';
 
 function DrumPad(props) {
+  const divRef = useRef(null);
   const [notes, setNotes] = useState(null);
   const [playing, setPlaying] = useState(false);
   const samples_map = {
         C3: '/audio/kicks/kick_1.wav',
         D3: '/audio/kicks/kick_2.wav',
-         E3:
+        E3: '/audio/snares/atlanta_snare.wav'
+  }
+
+ const playNote =(note)=> {
+    setNotes([{name: note}]);
+    setPlaying(true);
+  }
+
+  const stopNote =()=> {
+    setNotes(null);
+    setPlaying(false);
+  }
+
+  const handleKeyDown =(event)=>{
+    const map = {
+      "a": "C3",
+      "s": "D3",
+      "d": "E3"
     }
+    playNote(map[event.key])
+  }
+
+  const handleKeyUp =()=>{
+    stopNote();
+  }
+  
+  useEffect(() => {
+    divRef.current.focus();
+  }, []);
+
 
   return (
     <div>
-      <button class="drum-pad-element"
-        onMouseDown={()=>{
-            setNotes([{name: "C3"}]);
-            setPlaying(true);
-        }} 
-        onMouseUp={()=>{
-          setNotes(null);
-          setPlaying(false);
-        }}
-      >
-        Kick 1
-      </button>
+      <div ref={divRef} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex="0">
+        <button className="drum-pad-element"
+          onMouseDown={()=>playNote("C3")} 
+          onMouseUp={()=>stopNote()}
+          onKeyDown={(event)=>handleKeyDown(event)}
+        >
+          Kick 1
+        </button>
 
-      <button class="drum-pad-element"
-        onMouseDown={()=>{
-            setNotes([{name: "D3"}]);
-            setPlaying(true);
-        }} 
-        onMouseUp={()=>{
-          setNotes(null);
-          setPlaying(false);
-        }}
-      >
-        Kick 2
-      </button>
+        <button className="drum-pad-element"
+          onMouseDown={()=>playNote("D3")} 
+          onMouseUp={()=>stopNote()}
+        >
+          Kick 2
+        </button>
 
-        <button class="drum-pad-element"
-        onMouseDown={()=>{
-            setNotes([{name: "E3"}]);
-            setPlaying(true);
-        }} 
-        onMouseUp={()=>{
-          setNotes(null);
-          setPlaying(false);
-        }}
-      >
-        Snare 1
-      </button>
+        <button className="drum-pad-element"
+          onMouseDown={()=>playNote("E3")} 
+          onMouseUp={()=>stopNote()}
+        >
+          Snare 1
+        </button>
 
-      <Song isPlaying={playing} bpm={90}>
-        <Track steps={['C3']}>
-          {/* <Instrument type="synth" /> */}
-          <Instrument
-            type="sampler"
-            notes={notes}
-            samples={samples_map}
-            // onLoad={(buffers) => {
-              
-            // }}
-          />
-        </Track>
-      </Song>
+        <Song isPlaying={playing} bpm={90}>
+          <Track >
+            {/* <Instrument type="synth" /> */}
+            <Instrument
+              type="sampler"
+              notes={notes}
+              samples={samples_map}
+            />
+          </Track>
+        </Song>
+      </div>
     </div>
   )
 }
